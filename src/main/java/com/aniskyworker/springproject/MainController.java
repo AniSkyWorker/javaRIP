7abbd9208bc8171ec6365b733f4fe4b6102c094d
AniSkyWorker <holyrecycler@mail.ru>
2018-01-13 21:39:48 +0300

    добавил i18n и получение текста с сервиса

--- src/main/java/com/aniskyworker/springproject/MainController.java
+++ src/main/java/com/aniskyworker/springproject/MainController.java
@@ -1,5 +1,12 @@
 package com.aniskyworker.springproject;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Locale;
+import java.util.logging.Level;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import server.GuitarController;
 
 /**
- * Main controller of application
+ * Main controller of application.
  */
 @Controller
 public class MainController {
@@ -23,7 +30,7 @@ public class MainController {
    * Route add guitar page.
    */
   @RequestMapping(value = "/MainServlet", method = RequestMethod.POST)
-  public String addGuitar(final Model model, @RequestParam(value = "name", required = false) final String name,
+  public String addGuitar(final Locale locale,final Model model, @RequestParam(value = "name", required = false) final String name,
                           @RequestParam(value = "price", required = false) final String price,
                           @RequestParam(value = "board_stuff", required = false)final String boardStuff,
                           @RequestParam(value = "manufactureDate", required = false) final String manufactureDate) {
@@ -37,13 +44,42 @@ public class MainController {
    * Route show guitar list page.
    */
   @RequestMapping(value = "/MainServlet", method = RequestMethod.GET)
-  public String showGuitarList(final Model model,
+  public String showGuitarList(final Locale locale, final Model model,
                                @RequestParam(value = "show_guitars", required = false) final String showGuitars) {
     if (showGuitars == null) {
-      return addGuitar(model, null, null, null,null);
+      return addGuitar(locale, model, null, null, null,null);
     } else {
       model.addAttribute("guitarList", guitarController.getGuitars());
       return "guitarList";
     }
+  }
+
+  /**
+   * Route show guitar list page.
+   */
+  @RequestMapping(value = "/UrlText", method = RequestMethod.GET)
+  public String openUrlContent(final Locale locale, final Model model) {
+    String content = "";
+    try {
+      URL url = new URL("http://www.example.com/");
+      HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+      BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+      String line = "";
+      StringBuilder builder = new StringBuilder();
+
+      while ((line = in.readLine()) != null) {
+        builder.append(line);
+      }
+      in.close();
+      content = builder.toString();
+    } catch (Exception e) {
+      guitarController.log(Level.ALL, "Error when open remote url", e);
+      e.printStackTrace();
+    }
+
+    model.addAttribute("exampleContent", content);
+    return "example";
   }
 }
--- src/main/java/com/aniskyworker/springproject/SpringprojectApplication.java
+++ src/main/java/com/aniskyworker/springproject/SpringprojectApplication.java
@@ -1,18 +1,54 @@
 package com.aniskyworker.springproject;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-/*
-* Main application class
+/**
+ * Main application class.
  */
 @SpringBootApplication
-public class SpringprojectApplication {
+public class SpringprojectApplication extends WebMvcConfigurerAdapter {
 
-  /*
-  * Entry point of application
+  /**
+   * Entry point of application.
    */
   public static void main(final String[] args) {
     SpringApplication.run(SpringprojectApplication.class, args);
+  }
+
+  /**
+   * Locale setter.
+  */
+  @Bean
+  public LocaleResolver localeResolver() {
+    SessionLocaleResolver slr = new SessionLocaleResolver();
+    slr.setDefaultLocale(Locale.UK);
+    return slr;
+  }
+
+  /**
+   * Locale switcher.
+   */
+  @Bean
+  public LocaleChangeInterceptor localeChangeInterceptor() {
+    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+    lci.setParamName("lang");
+    return lci;
+  }
+
+  /**
+   * Add locale interceptor.
+   */
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(localeChangeInterceptor());
   }
 }
